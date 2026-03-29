9304a6ed1d2a4c855e9692d136d7bd3480344f85
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-28 21:16:23 -0500

    feat: initialize Spring Boot project with Maven wrapper and basic configuration

---
+++ src/test/java/com/isai/appisa/ApiBuenasPracticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isai.appisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBuenasPracticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
