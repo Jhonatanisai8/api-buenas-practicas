097704550db08da12863ad7b1c04f9614f5656a0
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-03-28 21:55:17 -0500

    feat: add transactional support to Cliente service methods

--- src/main/java/com/isai/appisa/service/impl/ClienteImpl.java
+++ src/main/java/com/isai/appisa/service/impl/ClienteImpl.java
@@ -3,6 +3,7 @@ package com.isai.appisa.service.impl;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.isai.appisa.models.dao.ClienteDao;
 import com.isai.appisa.models.entities.Cliente;
@@ -17,17 +18,20 @@ public class ClienteImpl implements ICliente {
     private final ClienteDao clienteDao;
 
     @Override
+    @Transactional
     public void eliminarCliente(UUID idCliente) {
         clienteDao.deleteById(idCliente);
     }
 
     @Override
+    @Transactional
     public Cliente guardarCliente(Cliente cliente) {
         return clienteDao.save(cliente);
 
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Cliente obtenerClientePorId(UUID idCliente) {
         return clienteDao.findById(idCliente).orElseThrow();
     }
